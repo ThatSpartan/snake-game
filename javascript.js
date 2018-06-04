939f8faad13c5604a5d12ffae949b14059b81750
ThatSpartan <dodo4931@outlook.com>
2018-06-04 14:38:45 -0400

    created a new document with classes

--- javascript.js
+++ javascript.js
@@ -1,269 +1,114 @@
-var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
-
-const fps = 3;
-
-
-let controller = {
-
-    select: 'right',
-
-    KeyPressed: event => {
-
-    // console.log(event.keyCode);
-
-        switch (event.keyCode) {
-
-            case 65: // left key
-                controller.select = "left";
-                break;
-
-            case 87: // up key
-                controller.select = "up";
-                break;
-
-            case 68: // right key
-                controller.select = "right";
-                break;
-
-            case 83: // down key
-                controller.select = "down";
-                break;
-
-            case 32: // space for testing
-                main_loop();
-                break;
-
-        }
-
-    }
-
-};
-
-let display = {
-
-    colors: {
-        head: '#ff7f00',
-        body: '#ffa500',
-        empty: '#868686',
-        fruit: '#ff6ff2',
-    },
-
-    block_size: 15,
-    space: 2,
-
-    Length(amount) { return amount * (this.block_size + this.space); },
-
-    DrawSquare(x, y, size, color) {
-
-        context.fillStyle = color;
-        context.fillRect(x, y, size, size);
-
-    },
-
-    DrawBoard(width, height) {
-
-        for(let row = 0; row < width; row++) {
-            for(let column = 0; column < height; column++) {
-
-                this.DrawSquare(this.Length(row), this.Length(column), this.block_size, this.colors.empty);
-
-            }
-        }
-        
-    },
-
-    DrawPlayer(player) {
-
-        this.DrawSquare(this.Length(player.body[0].x), this.Length(player.body[0].y), this.block_size, this.colors.head);
-
-        for(let p = 1; p < player.body.length; p++) {
-            
-            let piece = player.body[p];
-
-            this.DrawSquare(this.Length(piece.x), this.Length(piece.y), this.block_size, this.colors.body);
-
-        }
-
-    },
-
-    DrawFruit(fruit) {
-
-        this.DrawSquare(this.Length(fruit.x), this.Length(fruit.y), this.block_size, this.colors.fruit);
-
-    },
-
-};
-
-let game = {
-
-    width: 10,
-    height: 10,
-
-};
-
-let fruit = {
-
-    max_height: 0,
-    max_width: 0,
-
-    x: 0,
-    y: 0,
-
-    NewFruit() {
-
-        fruit.x = this.NewLoc(this.max_width);
-        fruit.y = this.NewLoc(this.max_height);
-
-    },
-
-    NewLoc(max, min=0) {
-
-        return Math.floor(Math.random() * (max - min)) + min;
-
-    },
-
-};
-
 let player = {
 
-    dx: 0,
-    dy: 0,
+	body: [ { x: 0, y: 0, }, { x: 0, y: 1, }, { x: 0, y: 2, }, { x: 0, y: 3, }, ],
 
-    body: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }, { x: 0, y: 5 }, { x: 0, y: 6 },],
+	get isAvailables() {
 
-    get new_x() { return this.body[0].x + this.dx; },
-    get new_y() { return this.body[0].y + this.dy; },
-    get new_coords() { return { x: this.new_x, y: this.new_y }; },
-    get x() { return this.body[0].x; },
-    get y() { return this.body[0].y; },
+		return 0;
 
-    // get availables() { return }
-
-    add() {
-        this.body.unshift({ x: this.new_x, y: this.new_y });
-    },
-
-    remove() {
-        this.body.pop();
-    },
-
-    move() {
-        this.add();
-        this.remove();
-    },
-
-    eat() {
-        this.add();
-        fruit.NewFruit();
-    },
+	},
 
 };
 
-let collisions = {
-    
-    Main(player, game, fruit) {
+class Display {
 
-        if (!collisions.Wall(player, game) && !collisions.Player(player)) {
+	constructor(game) {
 
-            if (collisions.Fruit(player, fruit)) {
-                player.eat();
-            } else {
-                player.move();
-            }
+		this.game = game;
 
-        }
+		this.block_size = 15;
+		this.offset = 2;
 
-    },
+		this.canvas = document.createElement("canvas");
+		this.context = canvas.getContext("2d");
 
-    Fruit(player, fruit) {
+		canvas.width = this.Size(game.width);
+		canvas.height = this.Size(game.height);
 
-        if (player.new_x == fruit.x && player.new_y == fruit.y) { return true; }
+		document.body.appendChild(canvas);
 
-        return false;
+	}
 
-    },
+	DrawPlayer() {
 
-    Wall(player, game) {
 
-        if (    player.new_x >= game.width ||
-                player.new_x < 0 ||
-                player.new_y >= game.height ||
-                player.new_y < 0
-        ) { return true; }
 
-        return false;
+	}
 
-    },
+	Size(size) { return size * (this.block_size + this.offset); }
 
-    Player(player) {
-
-        let current;
-        for(let index = 0; index < player.body.length; index++) {
-
-            current = player.body[index];
-
-            if (current.x == player.new_x && current.y == player.new_y) { return true; }
-
-        }
-
-        return false;
-
-    },
-
-};
-
-function main_loop() {
-    switch (controller.select) {
-        case 'right':
-
-            player.dx = 1;
-            player.dy = 0;
-
-            break;
-
-        case 'left':
-
-            player.dx = -1;
-            player.dy = 0;
-            
-            break;
-            
-        case 'up':
-            
-            player.dx = 0;
-            player.dy = -1;
-            
-            break;
-            
-        case 'down':
-            
-            player.dx = 0;
-            player.dy = 1;
-            
-            break;
-            
-    }
-
-    collisions.Main(player, game, fruit);
-
-    display.DrawBoard(game.width, game.height);
-    display.DrawPlayer(player);
-    display.DrawFruit(fruit);
-
-  setTimeout(() => {
-      console.log('new frame');
-      window.requestAnimationFrame(main_loop);
-  }, 1000 / fps);
 }
 
-canvas.width = display.Length(game.width);
-canvas.height = display.Length(game.height);
-document.body.appendChild(canvas);
+class Player {
 
-fruit.max_width = game.width;
-fruit.max_height = game.height;
-fruit.NewFruit();
+	constructor(game) {
 
-window.addEventListener("keydown", controller.KeyPressed);
-window.requestAnimationFrame(main_loop);
+		this.game = game;
+
+		this.body = [ { x: 0, y: 0, }, { x: 0, y: 1, }, { x: 0, y: 2, }, { x: 0, y: 3, }, { x: 1, y: 3, }, ];
+
+		this.dx = 1;
+		this.dy = 0;
+
+	}
+
+	get NewHead() { return { x: this.body[0].x + this.dx, y: this.body[0].y + this.dy }; }
+	get SnakeBody() { return this.body.map(z => z.x + z.y * this.game.width); }
+
+	Move() {
+
+		this.body.unshift( { x: this.NewHead.x, y: this.NewHead.y } );
+		this.body.pop();
+
+	}
+
+}
+
+class Fruit {
+
+	constructor() {
+
+		this.x, this.y;
+
+	}
+
+
+
+}
+
+class Game {
+
+	constructor() {
+
+		this.player = new Player(this);
+		this.fruit = new Fruit();
+
+		this.width = 10;
+		this.height = 10;
+
+	}
+
+	get Board() { return Array(this.width * this.height); }
+	get Empty() {
+
+		let array;
+
+		array = this.Board.filter(x => this.player.SnakeBody.contains(x));
+		console.log(array);
+
+		return array;
+
+	}
+
+}
+
+let main = () => {
+
+	let game = new Game();
+	// let display = new Display(game);
+
+	
+
+}
+
+main();
