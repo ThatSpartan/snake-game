a3de9d1d5a8a04c81c1da42056995cab731cd93f
ThatSpartan <dodo4931@outlook.com>
2018-06-04 21:18:14 -0400

    pretty much done

--- javascript.js
+++ javascript.js
@@ -1,14 +1,27 @@
-let player = {
+class Controller {
 
-	body: [ { x: 0, y: 0, }, { x: 0, y: 1, }, { x: 0, y: 2, }, { x: 0, y: 3, }, ],
+	constructor() {
 
-	get isAvailables() {
+		this.direction = 'right';
 
-		return 0;
+		document.addEventListener('keydown', this.KeyPressed);
 
-	},
+	}
 
-};
+	KeyPressed(event) {
+
+		switch (event.keyCode) {
+
+			case 87: this.direction = 'up'; break; // up
+			case 83: this.direction = 'down'; break; // down
+			case 68: this.direction = 'right'; break; // right
+			case 65: this.direction = 'left'; break; // left
+
+		}
+
+	}
+
+}
 
 class Display {
 
@@ -20,18 +33,49 @@ class Display {
 		this.offset = 2;
 
 		this.canvas = document.createElement("canvas");
-		this.context = canvas.getContext("2d");
+		this.context = this.canvas.getContext("2d");
 
-		canvas.width = this.Size(game.width);
-		canvas.height = this.Size(game.height);
+		this.canvas.width = this.Size(game.width);
+		this.canvas.height = this.Size(game.height);
 
-		document.body.appendChild(canvas);
+		document.body.appendChild(this.canvas);
+
+	}
+
+	DrawSquare(x, y, color) {
+
+		this.context.fillStyle = color;
+		this.context.fillRect( this.Size(x), this.Size(y), this.block_size, this.block_size);
+
+	}
+
+	DrawBoard() {
+
+		for (let row = 0; row < this.game.width; row++) {
+			for (var column = 0; column < this.game.height; column++) {
+
+				this.DrawSquare(row, column, '#709faf');
+
+			}
+		}
 
 	}
 
 	DrawPlayer() {
 
+		this.DrawSquare( this.game.player.body[0].x, this.game.player.body[0].y, '#46a4ff' );
 
+		for (let index = 1; index < this.game.player.body.length; index++) {
+
+			this.DrawSquare( this.game.player.body[index].x, this.game.player.body[index].y, '#204a87' );
+
+		}
+
+	}
+
+	DrawFruit() {
+
+		this.DrawSquare( this.game.fruit.x, this.game.fruit.y, '#31f288');
 
 	}
 
@@ -45,7 +89,16 @@ class Player {
 
 		this.game = game;
 
-		this.body = [ { x: 0, y: 0, }, { x: 0, y: 1, }, { x: 0, y: 2, }, { x: 0, y: 3, }, { x: 1, y: 3, }, ];
+		this.body = [];
+
+		this.dx = 1;
+		this.dy = 0;
+
+	}
+
+	Reset() {
+
+		this.body = [ { x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2, }, { x: 0, y: 3, }, { x: 1, y: 3, } ];
 
 		this.dx = 1;
 		this.dy = 0;
@@ -62,17 +115,59 @@ class Player {
 
 	}
 
+	Eat() {
+
+		this.body.unshift( { x: this.NewHead.x, y: this.NewHead.y } );
+
+	}
+
 }
 
 class Fruit {
 
 	constructor() {
 
-		this.x, this.y;
+		this.x = 0;
+		this.y = 0;
 
 	}
 
+	get location() { return this.x + ' . ' + this.y; }
 
+	NewFruit(coord) {
+
+		this.x = coord.x;
+		this.y = coord.y;
+
+	}
+
+}
+
+class Collision {
+
+	ColliedWithWall(player, game_dimention) {
+
+		return (player.NewHead.x == game_dimention.x && player.NewHead.y == game_dimention.y);
+
+	}
+
+	ColliedWithBody(player) {
+
+		for (let index = 0; index < player.body.length; index++) {
+
+			if (player.NewHead.x == player.body[index].x && player.NewHead.y == player.body[index].y) { return true; }
+
+		}
+
+		return false;
+
+	}
+
+	ColliedWithFruit(player, fruit) {
+
+		return (player.NewHead.x == fruit.x && player.NewHead.y == fruit.y);
+
+	}
 
 }
 
@@ -80,35 +175,126 @@ class Game {
 
 	constructor() {
 
-		this.player = new Player(this);
-		this.fruit = new Fruit();
-
 		this.width = 10;
 		this.height = 10;
 
+		this.player = new Player(this);
+		this.fruit = new Fruit();
+		this.display = new Display(this);
+		this.controller = new Controller();
+		this.collision = new Collision();
+
 	}
 
-	get Board() { return Array(this.width * this.height); }
-	get Empty() {
+	NewGame() {
 
-		let array;
+		this.player.Reset();
+		this.fruit.NewFruit( this.GetEmptyCoord );
 
-		array = this.Board.filter(x => this.player.SnakeBody.contains(x));
-		console.log(array);
+		this.loop();
 
-		return array;
+	}
+
+	get GetEmptyCoord() {
+
+		let x, y;
+
+		do {
+
+			x = this.GetInt(this.width);
+			y = this.GetInt(this.height);
+
+		} while (!this.Empty( { x: x, y: y } ));
+
+		return { x: x, y: y };
 
 	}
 
 }
+
+Game.prototype.Empty = function(coord) {
+
+	let el = this.player.body;	
+
+	for (var i = 0; i < el.length; i++) {
+
+		if (el[i].x == coord.x && el[i].y == coord.y) {
+			return false;
+		}
+
+	}
+
+	return true;
+
+};
+
+Game.prototype.GetInt = function(max=0, min=0) {
+
+	return Math.floor(Math.random() * (max - min)) + min;
+
+};
+
+Game.prototype.SetDirection = function() {
+
+	switch (this.controller.direction) {
+
+		case 'up':
+			this.player.dx = 0;
+			this.player.dy = -1;
+			break;
+	
+		case 'down':
+			this.player.dx = 0;
+			this.player.dy = 1;
+			break;
+
+		case 'right':
+			this.player.dx = 1;
+			this.player.dy = 0;
+			break;
+
+		case 'left':
+			this.player.dx = -1;
+			this.player.dy = 0;
+			break;
+		
+	}
+
+};
+
+Game.prototype.loop = function() {
+console.log('hey');
+	this.display.DrawBoard();
+	this.display.DrawPlayer();
+	this.display.DrawFruit();
+
+	this.SetDirection();
+
+	if ( !this.collision.ColliedWithWall( this.player, { x: this.width, y: this.height } ) || !this.collision.ColliedWithBody( this.player ) ) {
+
+		if ( this.collision.ColliedWithFruit( this.player, this.fruit ) ) {
+
+			this.player.Eat();
+			this.fruit.NewFruit(this.GetEmptyCoord);
+
+		} else {
+
+			this.player.Move();
+
+		}
+
+	}
+
+	window.requestAnimationFrame( () => { this.loop(); } );	
+
+};
 
 let main = () => {
 
 	let game = new Game();
-	// let display = new Display(game);
 
-	
+	game.NewGame();
 
-}
+};
 
 main();
